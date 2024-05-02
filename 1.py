4c05f1096784b8fb3b079f6c5829ed006926c88d
GorbovAlex <168721569+GorbovAlex@users.noreply.github.com>
2024-05-02 12:17:15 +0300

    add template

---
+++ 1.py
@@ -0,0 +1,44 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print ("hello, world
+       
+SyntaxError: incomplete input
+>>> print ("hello, Valera")
+...        
+hello, Valera
+>>> print ("you are shit")
+...        
+you are shit
+>>> print (" i love you ')
+...        
+SyntaxError: incomplete input
+>>> SyntaxError: incomplete input
+...        
+SyntaxError: incomplete input
+>>> print ("
+...        
+SyntaxError: incomplete input
+>>> SyntaxError: incomplete input
+...        
+SyntaxError: incomplete input
+>>> print ("A=7st")
+...        
+A=7st
+>>> print ("B=?st")
+...        
+B=?st
+>>> print ("S=?st2")
+...        
+S=?st2
+>>> print ("P=?st")
+...        
+P=?st
+>>> print ("1)")
+...        
+1)
+>>> print ("2)")
+...        
+2)
+>>> print ("ответ=_____st,________st, ________st2.")
+...        
+ответ=_____st,________st, ________st2.
